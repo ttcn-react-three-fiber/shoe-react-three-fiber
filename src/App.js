c35b532749376623674c75320f1410b68c181fc4
tvhien2000 <tvhien2000az@gmail.com>
2021-05-23 16:03:45 +0700

    aaaa

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Suspense, useRef, useState, useEffect } from "react"
+import { Canvas, useFrame } from "@react-three/fiber"
+import { ContactShadows, Environment, useGLTF, OrbitControls } from "@react-three/drei"
+import { HexColorPicker } from "react-colorful"
+import { proxy, useSnapshot } from "valtio"
 
-function App() {
+import ShoeModel from './ShoeModel'
+import Picker from "./Picker"
+
+// Using a Valtio state model to bridge reactivity between
+// the canvas and the dom, both can write to it and/or react to it.
+   // laces: "#ffffff",
+    // mesh: "#ffffff",
+    // caps: "#ffffff",
+    // inner: "#ffffff",
+    // sole: "#ffffff",
+    // stripes: "#ffffff",
+    // band: "#ffffff",
+    // patch: "#ffffff",
+const state = proxy({
+  count:0,
+  current: null,
+  items: {},
+})
+console.log( state.items);
+
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Canvas shadows dpr={[1, 2]} camera={{ position: [0, 0, 4], fov: 50 }}>
+        <ambientLight intensity={0.7} />
+        <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
+        <Suspense fallback={null}>
+          <ShoeModel  state={state}/>
+          <Environment preset="city" />
+          <ContactShadows rotation-x={Math.PI / 2} position={[0, -0.8, 0]} opacity={0.25} width={10} height={10} blur={1.5} far={0.8} />
+        </Suspense>
+        <OrbitControls  maxPolarAngle={Math.PI / 2} enableZoom={false} enablePan={false} />
+      </Canvas>
+      <Picker state={state}/>
+    </>
+  )
 }
-
-export default App;
